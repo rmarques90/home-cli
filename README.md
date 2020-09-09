2d2808718c775a27d425404cae1b30d52efe77c9
Rafael Marques <rafamdd@gmail.com>
2020-09-09 03:22:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Home-CLI for eWeLink
+
+Application developed to manipulate your devices in your computer in an interactive CLI.
+
+Just create an `.env` file with the properties on model, then `yarn install` and `node index.js` to run the application.
+
+eWeLink Docs: https://ewelink-api.now.sh/docs/introduction
+
---
+++ index.js
@@ -0,0 +1,133 @@
+#!/bin/env node
+
+const inquirer = require('inquirer');
+const ewelink = require('ewelink-api');
+const Table = require('cli-table');
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+require('dotenv').config();
+
+//conexao com o ewelink
+const connection = new ewelink({
+    email: process.env.EWELINK_EMAIL,
+    password: process.env.EWELINK_PASSWORD,
+    region: process.env.EWELINK_REGION
+});
+
+//função para gerar tabela
+const showDevicesTable = (devicesData) => {
+    const table = new Table({
+        head: [chalk.cyan('Name'), chalk.cyan('Status')],
+        colWidths: [15, 15]
+    });
+
+    if (devicesData) {
+        devicesData.map((d) => {
+            if (d.params && d.params.switches) {
+                d.params.switches.forEach(s => {
+                    if ((d.name === 'Cozinha' || d.name === 'Sala') && s.outlet < 2) { //soh tenho switch com 2 botoes e o infeliz me retorna 4
+                        table.push([
+                            `${d.name} - ${s.outlet}`, (s.switch && s.switch === 'on' ? chalk.green('On') : chalk.red('Off'))
+                        ])
+                    }
+                })
+            } else if (d.params && d.params.switch) {
+                table.push([
+                    d.name, (d.params && d.params.switch && d.params.switch === 'on' ? chalk.green('On') : chalk.red('Off'))
+                ])
+            }
+        });
+    }
+
+    console.log(table.toString());
+};
+
+const getDevices = async () => {
+    return await connection.getDevices();
+};
+
+const listDevices = async () => {
+    let devices = await getDevices();
+    showDevicesTable(devices);
+};
+
+const mapDevicesToChoices = (devices) => {
+    if (!devices || !devices.length) {
+        return;
+    }
+
+    let arrayToReturn = [];
+    devices.map(d => {
+        if (d.params && d.params.switches) {
+            d.params.switches.forEach(s => {
+                if ((d.name === 'Cozinha' || d.name === 'Sala') && s.outlet < 2) {
+                    arrayToReturn.push({
+                        name: `${d.name} - Channel: ${s.outlet + 1}`,
+                        value: {id: d.deviceid, channel: (s.outlet + 1)}
+                    })
+                }
+            });
+        } else if (d.params && d.params.switch) {
+            arrayToReturn.push({
+                name: d.name,
+                value: {id: d.deviceid}
+            })
+        }
+    });
+    return arrayToReturn;
+
+};
+
+const initialOptionsInquirer = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'What do you wanna do?',
+            choices: [
+                {name: 'List all devices status', value: 1},
+                {name: 'Toggle some device', value: 2}
+            ]
+        }
+    ])
+        .then( async answer => {
+            if (answer.option === 1) {
+                await listDevices();
+            } else if (answer.option === 2) {
+                let devices = await getDevices();
+                let choices = mapDevicesToChoices(devices);
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'selectedDevice',
+                        choices: choices
+                    }
+                ]).then(async answer => {
+                    if (answer && answer.selectedDevice) {
+                        let response;
+                        if (answer.selectedDevice.id && answer.selectedDevice.channel == undefined) {
+                            response = await connection.toggleDevice(`${answer.selectedDevice.id}`);
+                        } else {
+                            response = await connection.toggleDevice(`${answer.selectedDevice.id}`, answer.selectedDevice.channel);
+                        }
+                        if (response && response.status === 'ok') {
+                            console.log(chalk.blue(`Device status updated!`));
+                        } else {
+                            console.log(chalk.red('error', response));
+                        }
+                    }
+                })
+            }
+        })
+};
+
+const init = () => {
+    //texto inicial
+    console.log(chalk.green(figlet.textSync('eWeLink Home CLI')));
+
+    initialOptionsInquirer();
+};
+
+init();
